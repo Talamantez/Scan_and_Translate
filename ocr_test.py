5235ccddbc96af5ab3933c2dd9097930acb98269
Robert Talamantez <robert.m.talamantez@gmail.com>
2023-12-28 06:59:56 -0800

    small refactor

--- ocr_test.py
+++ ocr_test.py
@@ -2,18 +2,19 @@ from PIL import Image
 import pytesseract
 import numpy as np
 from translate import Translator
-import re
 
-
+# latin = 'lat', russian = 'rus', english = 'en'
 source_lang = 'lat'
 target_lang = 'en'
 translator = Translator(to_lang=target_lang, from_lang=source_lang)
 # get currently supported Tesseract OCR languages
 # print(pytesseract.get_languages())
-filename = 'C:/Users/rober/Documents/Personal Projects/OCR/Russian_Revolution/test_image.png'
-# filename = 'C:/Users/rober/Documents/Personal Projects/OCR/Russian_Revolution/test_image_2.png'
-# filename = 'C:/Users/rober/Documents/Personal Projects/OCR/Russian_Revolution/october.png'
-# filename = 'C:/Users/rober/Documents/Personal Projects/OCR/Russian_Revolution/march_17_russian_revolution_crop3.png'
+
+root = 'C:/Users/rober/Documents/Personal Projects/OCR/Scan_and_Translate/'
+filename = root + 'test_image_6.png'
+# filename = root + 'test_image_2.png'
+# filename = root + 'october.png'
+# filename = root + 'march_17_russian_revolution_crop3.png'
 
 # open the file
 img1 = np.array(Image.open(filename))
@@ -27,7 +28,7 @@ print("Character Count: ")
 print(len(text))
 
 # Truncating
-limit = 12
+limit = 10
 print('limiting character length to translate to ')
 print(limit)
 truncated_text = text[:limit] + '...' if len(text) > limit else text
@@ -36,5 +37,5 @@ print("OCR Latin: ")
 print(text)
 
 translated_text = translator.translate(truncated_text)
-print("Translated English")
+print("Translated English:")
 print(translated_text)
