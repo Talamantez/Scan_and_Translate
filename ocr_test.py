3bdde584af15cf77483f73d368421af209dd54ea
Robert Talamantez <robert.m.talamantez@gmail.com>
2023-12-29 14:36:49 -0800

    catch exceptions, process 1 word at a time

--- ocr_test.py
+++ ocr_test.py
@@ -3,15 +3,40 @@ import pytesseract
 import numpy as np
 from translate import Translator
 
+def translateWord(word):
+    result = ""
+    try:
+        result = translator.translate(word)
+    except:
+        result = "ERR"
+    return result
+
+def translateText(text):
+    translated_text = []
+    # Split into words
+    wordArray = text.split()
+    for word in wordArray:
+        
+        myWord = word.split(sep=",")[0]
+
+        translated_word = translateWord(myWord)
+
+        # print(translated_word)
+        translated_text.append(translated_word)
+    separator = " "
+    return separator.join(translated_text)
+        
+
 # latin = 'lat', russian = 'rus', english = 'en'
-source_lang = 'lat'
+source_lang = 'rus'
 target_lang = 'en'
 translator = Translator(to_lang=target_lang, from_lang=source_lang)
 # get currently supported Tesseract OCR languages
 # print(pytesseract.get_languages())
 
 root = 'C:/Users/rober/Documents/Personal Projects/OCR/Scan_and_Translate/'
-filename = root + 'test_image_6.png'
+# filename = root + 'test_image_6.png'
+filename = root + 'Screenshot_20231229_130556_Instagram.jpg'
 # filename = root + 'test_image_2.png'
 # filename = root + 'october.png'
 # filename = root + 'march_17_russian_revolution_crop3.png'
@@ -27,15 +52,10 @@ text = pytesseract.image_to_string(img1, lang=source_lang)
 print("Character Count: ")
 print(len(text))
 
-# Truncating
-limit = 10
-print('limiting character length to translate to ')
-print(limit)
-truncated_text = text[:limit] + '...' if len(text) > limit else text
-
-print("OCR Latin: ")
+# Print OCR text
+print("OCR " + source_lang.capitalize() + ":")
 print(text)
 
-translated_text = translator.translate(truncated_text)
-print("Translated English:")
-print(translated_text)
+# Print Translated text
+print("Translated " + target_lang.capitalize() + ":")
+print(translateText(text))
